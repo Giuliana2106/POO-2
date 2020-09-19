7ca9d121df71d7c022c0b962318f889fad582af6
Giuliana2106 <62736506+Giuliana2106@users.noreply.github.com>
2020-09-19 16:22:27 -0600

    Primera Parte Terminada

---
+++ Agenda/Menú.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agenda
+{
+    class Menú
+    {
+        private int op = 0;
+
+        public int ListaMenú()
+        {
+            Console.WriteLine("1.-Crear Contacto");
+            Console.WriteLine("2.-Buscar Contacto");
+            Console.WriteLine("3.-Modificar Contacto");
+            Console.WriteLine("4.-Eliminar Contacto");
+            Console.WriteLine("5.Salir");
+            Console.WriteLine("opción: ");
+            op = int.Parse(Console.ReadLine());
+            return op;     
+        }
+    }
+}
---
+++ Agenda/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agenda
+{
+    class Program
+    {
+        static string[,] agenda = new string[4, 10];
+        static int Contador = 0;
+        static Menú m = new Menú();
+        static void Main(string[] args)
+        {
+            agenda = new string[4, 10];
+
+            int opciones = 0;
+
+            while (opciones != 5)
+            {
+                opciones = m.ListaMenú();
+
+                if (opciones == 1)
+                {
+
+                }
+
+                else if (opciones == 2)
+                {
+
+                }
+
+                else if (opciones == 3)
+                {
+
+                }
+
+                else if (opciones == 4)
+                {
+
+                }
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
